cee965286858166e2deeda9e4a0ce16d916e2457
Jeff Papiez <jpapiez@live.com>
2024-08-24 10:49:49 -0700

    Adding CB2 config

---
+++ build-cb1.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -f "./src/config" ]]; then
+	rm "./src/config"
+fi
+cat "./config/default" >> ./src/config
+cat "./config/armbian/default" >> ./src/config
+cat "./config/armbian/CB2" >> ./src/config
+source ./src/config
+IMGCOUNT=$(ls ./src/image/*Debian11_minimal_kernel*.img.xz | wc -l)
+if [ $IMGCOUNT -eq 0 ]; then
+	echo "Downloading image..."
+	find ./src/image -type f -not -name '.gitkeep' -delete
+	aria2c -d ./src/image --seed-time=0 $DOWNLOAD_URL_IMAGE
+fi
+exit 0
+pushd ./src
+sudo bash -x ./build_dist
+popd
+rm "./src/config"
